00b8bcd1c8ed6957a43baa05cdcc281022e72f55
okazakikeita <ca01254004@st.kawahara.ac.jp>
2026-04-08 17:17:50 +0900

    SEの追加

---
+++ Game/SE.h
@@ -0,0 +1,18 @@
+﻿#pragma once
+enum SE
+{
+	SE_umbrellaSpinning,	//傘回しの音
+	SE_run,					//走る音
+	SE_ball,				//ボールを落とす音
+	SE_teaBowl,				//茶碗が割れる音
+	SE_masu,				//升を落とした音
+	SE_penguin,				//ペンギンの足音
+	SE_bone,				//骨が崩れる音
+	SE_spotlight,			//スポットライト
+	SE_cheers,				//歓声
+	SE_crackedEgg,			//卵が割れる音
+	SE_booing,				//ブーイング
+	SE_hitHead,				//頭を打つ音
+	SE_smartphone,			//スマホ落下音
+};
+
