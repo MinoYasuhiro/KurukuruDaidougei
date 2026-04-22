98b88f27dc8c629ed4159cea8d6641fc6fb17543
AokiMutsuki <ca01254001@st.kawahara.ac.jp>
2026-04-22 17:26:48 +0900

    Aoki mutsuki/game over to title (#55)
    * Pauseメニューの修正
    
    * GameClearの修正
    
    * ゲームオーバーの修正
    
    * Gameの修正
    
    * ゲームクリアからタイトルの遷移の追加
    
    * ゲームクリアからタイトルの遷移の修正
    
    * プレイヤーのスタートポジションの修正
    
    * プレイヤーの差分
    
    * Playerの差分の修正
    
    * ゲームオーバーからタイトル画面への遷移
    
    ---------
    
    Co-authored-by: AokiMutsuki <aokiMutsuki@gmail.com>

--- Game/GameClear.cpp
+++ Game/GameClear.cpp
@@ -1,12 +1,19 @@
-
 #include "stdafx.h"
 #include "GameClear.h"
 #include "Title.h"
 #include "Game.h"
 
+
 bool GameClear::Start()
 {
     m_SpriteRender.Init("Assets/Sprite/GameClear.DDS", 1920, 1080);
+
+    m_RetryRender.Init("Assets/Sprite/Retry.DDS", 500, 500);
+    m_RetryRender.SetPosition(Vector3(0, -200, 0)); // ★ 手前
+    m_RetryRender.SetScale(Vector3(1.5f, 1.5f, 1.5f));
+
+    m_canInput = false;
+
     return true;
 }
 
@@ -14,8 +21,29 @@ bool GameClear::Start()
 void GameClear::Update()
 {
     m_SpriteRender.Update();
+    m_RetryRender.Update();
 
-    // 3秒経過したらタイトルへ
+    // ★ 入力持ち越し対策
+    if (!m_canInput)
+    {
+        m_canInput = true;
+        return;
+    }
+
+
+    // Retry
+    if (g_pad[0]->IsPress(enButtonA))
+    {
+        if (Game* game = FindGO<Game>("game"))
+        {
+            game->ResetGame();
+        }
+        DeleteGO(this);
+        return;
+    }
+
+    
+    // タイトル
     if (g_pad[0]->IsPress(enButtonB))
     {
         if (Game* game = FindGO<Game>("game"))
@@ -31,4 +59,7 @@ void GameClear::Update()
 void GameClear::Render(RenderContext& rc)
 {
     m_SpriteRender.Draw(rc);
+  
+    m_RetryRender.Draw(rc);
+
 }
