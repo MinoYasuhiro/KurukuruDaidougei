800f4bdc04a4a2b6ff52480b7933adbe1f900c7f
OkazakiKeita <ca01254004@st.kawahara.ac.jp>
2026-06-04 11:16:00 +0900

    ポーズ画面とサウンド設定画面上での操作の説明を追加 (#127)

--- Game/Pause.h
+++ Game/Pause.h
@@ -16,6 +16,7 @@ private:
 	SpriteRender m_SpriteRender;
 	SpriteRender m_Sprite;
 	SpriteRender m_PauseRender;
+	SpriteRender m_pauseOperation;
 	Vector3 m_position;
 	Pause* m_pause;
 	MenuUI* m_menuUI = nullptr;
