3d013d2da862302b342b0ea3072593857c6ca68c
AokiMutsuki <ca01254001@st.kawahara.ac.jp>
2026-06-05 21:41:49 +0900

    Aoki mutsuki/operation (#129)
    * Pauseメニューの修正
    
    * GameClearの修正
    
    * ゲームオーバーの修正
    
    * Gameの修正
    
    * ゲームクリアからタイトルの遷移の追加
    
    * ゲームクリアからタイトルの遷移の修正
    
    * プレイヤーのスタートポジションの修正
    
    * プレイヤーの差分
    
    * Playerの差分の修正
    
    * ゲームオーバーからタイトル画面への遷移
    
    * ゲームオーバーからタイトル画面に移行するものの追加
    
    * ゲームオーバーからリトライ画面への遷移
    
    * ゲームオーバーからリトライ画面の修正
    
    * SoundTestの追加
    
    * SoundTestの修正
    
    * Gameの修正
    
    * SoundTestの修正２
    
    * SoundTestの修正３
    
    * .h,.cppの修正
    
    * SoundTestの修正
    
    * SoundTestの修正
    
    * SoundTestの修正3
    
    * SoundTestのアセットの追加
    
    * カメラの拡大、縮小の追加
    
    * GameCameraの修正
    
    * GameCamera.ｈの修正
    
    * Gameの修正
    
    * タイトルからサウンドオプションへの遷移（Xボタンで遷移）
    
    * QTEの追加及び、Game内のフェーズの追加と細かな修正
    
    * Gameの修正
    
    * QTEの修正
    
    * QTEの修正１
    
    * QTEの修正
    
    * Sound本背景の実装
    
    * Sound背景アセット
    
    * GameClear本画面のアセットの追加
    
    * PauseMenuの背景の本実装と、文字フォントの変更
    
    * 本番用のステージの追加　
    
    * ポートフォリオの追加
    
    * ポートフォリオの修正
    
    * Youtubeリンクの追加
    
    * ポートフォリオの修正
    
    * htmlファイルの追加
    
    * 操作説明の追加（後にルール説明も追加）
    
    ---------
    
    Co-authored-by: AokiMutsuki <aokiMutsuki@gmail.com>

--- Game/Title.cpp
+++ Game/Title.cpp
@@ -3,11 +3,18 @@
 #include "Game.h"
 #include "SoundOption.h"
 #include "BGMManager.h"
+#include "Operation.h"
+
+namespace {
+
+	const Vector3 TITLE_POS = { 0.0f,0.0f,0.0f };
+	
+}
 
 bool Title::Start()
 {
 	m_spriteRender.Init("Assets/Sprite/Title.DDS", 1920, 1080);
-	m_spriteRender.SetPosition({ 0, 0, 0 });
+	m_spriteRender.SetPosition({TITLE_POS});
 	m_canInput = false;
 
 	m_BGM = FindGO<BGMManager>("bgmManager");
@@ -16,43 +23,53 @@ bool Title::Start()
 		m_BGM = NewGO<BGMManager>(0, "bgmManager");
 		m_BGM->Init();
 	}
+	
 
 	m_BGM->Play(BGM_GameClear);
 
 	return true;
 }
 
+
 void Title::Update()
 {
+    m_spriteRender.Update();
 
-	m_spriteRender.Update();
+    if (!m_canInput)
+    {
+        m_canInput = true;
+        return;
+    }
 
-
-	if (!m_canInput)
-	{
-		m_canInput = true;
-		return; // ★ 1フレーム入力無視
-	}
-
-	//…のちに統合版に加える…//
-	if (g_pad[0]->IsPress(enButtonSelect))
-	{
-		if (Game* game = FindGO<Game>("game"))
-		{
-			game->ResetGame();
-		}
-		DeleteGO(this);
-	}
+    //…のちに統合版に加える…//
+    if (g_pad[0]->IsPress(enButtonSelect))
+    {
+	    if (Game* game = FindGO<Game>("game"))
+	    {
+		game->ResetGame();
+	    }
+	    DeleteGO(this);
+    }
 
 
-	// ★ サウンドオプションへ（Xボタン）
-	if (g_pad[0]->IsPress(enButtonX))
-	{
-		Game::SetState(GameState::Pause);
-		NewGO<SoundOption>(0, "soundOption");
-		DeleteGO(this);
-	}
+    // ★ 操作説明へ（Yボタン）
+    if (g_pad[0]->IsPress(enButtonY))
+    {
+    	Game::SetState(GameState::Title);
+        m_operation = NewGO<Operation>(0, "operation");
+	    DeleteGO(this);
+    }
 
+    // ★ サウンドオプションへ（Xボタン）
+    if (g_pad[0]->IsPress(enButtonX))
+    {
+
+	   Game::SetState(GameState::Title);   // ← 超重要
+	   Game::SetState(GameState::SoundTest);   // ← 正しい状態
+
+	   NewGO<SoundOption>(0, "soundOption");
+	   DeleteGO(this);
+    }   
 }
 
 void Title::Render(RenderContext& rc)
