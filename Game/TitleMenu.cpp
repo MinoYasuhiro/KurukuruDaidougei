17450f94ad0cbcef48e212ad449f36d7a2d97ddb
AokiMutsuki <ca01254001@st.kawahara.ac.jp>
2026-06-11 01:31:57 +0900

    Aoki mutsuki/operation (#136)
    * Pauseメニューの修正
    
    * GameClearの修正
    
    * ゲームオーバーの修正
    
    * Gameの修正
    
    * ゲームクリアからタイトルの遷移の追加
    
    * ゲームクリアからタイトルの遷移の修正
    
    * プレイヤーのスタートポジションの修正
    
    * プレイヤーの差分
    
    * Playerの差分の修正
    
    * ゲームオーバーからタイトル画面への遷移
    
    * ゲームオーバーからタイトル画面に移行するものの追加
    
    * ゲームオーバーからリトライ画面への遷移
    
    * ゲームオーバーからリトライ画面の修正
    
    * SoundTestの追加
    
    * SoundTestの修正
    
    * Gameの修正
    
    * SoundTestの修正２
    
    * SoundTestの修正３
    
    * .h,.cppの修正
    
    * SoundTestの修正
    
    * SoundTestの修正
    
    * SoundTestの修正3
    
    * SoundTestのアセットの追加
    
    * カメラの拡大、縮小の追加
    
    * GameCameraの修正
    
    * GameCamera.ｈの修正
    
    * Gameの修正
    
    * タイトルからサウンドオプションへの遷移（Xボタンで遷移）
    
    * QTEの追加及び、Game内のフェーズの追加と細かな修正
    
    * Gameの修正
    
    * QTEの修正
    
    * QTEの修正１
    
    * QTEの修正
    
    * Sound本背景の実装
    
    * Sound背景アセット
    
    * GameClear本画面のアセットの追加
    
    * PauseMenuの背景の本実装と、文字フォントの変更
    
    * 本番用のステージの追加　
    
    * ポートフォリオの追加
    
    * ポートフォリオの修正
    
    * Youtubeリンクの追加
    
    * ポートフォリオの修正
    
    * htmlファイルの追加
    
    * 操作説明の追加（後にルール説明も追加）
    
    * 操作説明のアセットの追加
    
    * 操作画面の修正
    
    * ビルド内に追加（動作確認済み）
    
    * 操作説明の修正とタイトルのメニューと選択できるように追加
    
    ---------
    
    Co-authored-by: AokiMutsuki <aokiMutsuki@gmail.com>

---
+++ Game/TitleMenu.cpp
@@ -0,0 +1,112 @@
+#include "stdafx.h"
+#include "TitleMenu.h"
+
+
+namespace {
+    const Vector3 START_POS = {  0.0f, -250.0f, 0.0f }; // 左下
+    const Vector3 SOUND_POS = {  600.0f, -250.0f, 0.0f }; // 右下
+    const Vector3 OP_POS = { -600.0f,   -250.0f, 0.0f }; // 左上
+    //const Vector3 RULE_POS = { 300.0f,   50.0f, 0.0f }; // 右上 (未実装)
+}
+
+
+
+bool TitleMenu::Start()
+{
+    m_selectIndex = 0;
+    m_result = MenuResult::None;
+
+    m_startRender.Init("Assets/Sprite/GameStart.DDS", 300, 100);
+    m_startRender.SetPosition(START_POS);
+
+    m_soundRender.Init("Assets/sprite/Soundtest.dds", 500, 300);
+    m_soundRender.SetPosition(SOUND_POS);
+
+    m_operationRender.Init("Assets/Sprite/OperationButton.DDS", 300, 100);
+    m_operationRender.SetPosition(OP_POS);
+
+    m_inputTimer = 0.12f;   
+    m_skipInputFrame = true;
+
+    return true;
+}
+
+void TitleMenu::Update()
+{
+    m_inputTimer -= 1.0f / 60.0f;
+
+    //クールタイム中は入力無効
+    if (m_inputTimer > 0.0f)return;
+
+    if (m_skipInputFrame)
+    {
+        m_skipInputFrame = false;
+        return; // ★このフレームは入力受けない
+    }
+
+    if (m_result != MenuResult::None)
+    {
+        return; 
+    }
+
+    Input();
+    SelectScale();
+
+    m_startRender.Update();
+    m_soundRender.Update();
+    m_operationRender.Update();
+}
+
+
+void TitleMenu::Input()
+{
+   
+
+    if (g_pad[0]->IsPress(enButtonLeft))
+    {
+        m_selectIndex--;
+        if (m_selectIndex < 0) m_selectIndex = 2;
+
+        m_inputTimer = m_inputinterval; 
+    }
+
+    if (g_pad[0]->IsPress(enButtonRight))
+    {
+        m_selectIndex++;
+        if (m_selectIndex > 2) m_selectIndex = 0;
+
+        m_inputTimer = m_inputinterval; 
+    }
+
+    if (g_pad[0]->IsPress(enButtonA))
+    {
+        switch (m_selectIndex)
+        {
+        case 0: m_result = MenuResult::Operation; break;
+        case 1: m_result = MenuResult::GameStart; break;
+        case 2: m_result = MenuResult::SoundOption; break;
+        }
+
+        m_inputTimer = m_inputinterval; 
+    }
+}
+
+void TitleMenu::SelectScale()
+{
+    // 全体リセット
+    m_startRender.SetScale({ 1,1,1 });
+    m_soundRender.SetScale({ 1,1,1 });
+    m_operationRender.SetScale({ 1,1,1 });
+
+    // 選択中だけ拡大
+    if (m_selectIndex == 0) m_operationRender.SetScale({ 1.2f,1.2f,1.2f });
+    if (m_selectIndex == 1) m_startRender.SetScale({ 1.2f,1.2f,1.2f });
+    if (m_selectIndex == 2) m_soundRender.SetScale({ 1.2f,1.2f,1.2f });
+}
+
+void TitleMenu::Render(RenderContext& rc)
+{
+    m_startRender.Draw(rc);
+    m_soundRender.Draw(rc);
+    m_operationRender.Draw(rc);
+}
