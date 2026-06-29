6fdc8f7e00697e986a4dbcce7f542f4a4193d244
OkazakiKeita <ca01254004@st.kawahara.ac.jp>
2026-06-29 11:58:04 +0900

    QTEの制限時間の可視化 (#165)

---
+++ Game/QTETimerUI.h
@@ -0,0 +1,30 @@
+﻿#pragma once
+class QTETimerUI:public IGameObject
+{
+public:
+	QTETimerUI() {};
+	~QTETimerUI() {};
+
+	bool Start()override;
+	void Update()override;
+	void Render(RenderContext& renderContext)override;
+
+	void StartQTE(float limitTime);		//タイマー起動関数
+	bool IsTimeOut();					//時間切れ確認関数
+	float GetProgress();				//進行度取得関数
+	void SetProgress(float rate);		//割合を引数として受け取る関数
+
+private:
+	//時間を管理する変数
+	float m_maximumTime = 0.0f;
+	float m_remainingTime = 0.0f;
+	bool m_activeFlag = false;
+
+	//画面・画像を管理する変数
+	float m_displayX = 0.0f;
+	float m_displayY = 0.0f;
+	float m_maximumWidth = 0.0f;
+	SpriteRender m_frame;
+	SpriteRender m_colorBars;
+};
+
