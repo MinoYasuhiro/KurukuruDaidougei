4221d893117cf63fba74d6a020b5f6be4d0beedf
AokiMutsuki <ca01254001@st.kawahara.ac.jp>
2026-06-29 16:12:02 +0900

    Aoki mutsuki/loading (#166)
    * Pauseメニューの修正
    
    * GameClearの修正
    
    * ゲームオーバーの修正
    
    * Gameの修正
    
    * ゲームクリアからタイトルの遷移の追加
    
    * ゲームクリアからタイトルの遷移の修正
    
    * プレイヤーのスタートポジションの修正
    
    * プレイヤーの差分
    
    * Playerの差分の修正
    
    * ゲームオーバーからタイトル画面への遷移
    
    * ゲームオーバーからタイトル画面に移行するものの追加
    
    * ゲームオーバーからリトライ画面への遷移
    
    * ゲームオーバーからリトライ画面の修正
    
    * SoundTestの追加
    
    * SoundTestの修正
    
    * Gameの修正
    
    * SoundTestの修正２
    
    * SoundTestの修正３
    
    * .h,.cppの修正
    
    * SoundTestの修正
    
    * SoundTestの修正
    
    * SoundTestの修正3
    
    * SoundTestのアセットの追加
    
    * カメラの拡大、縮小の追加
    
    * GameCameraの修正
    
    * GameCamera.ｈの修正
    
    * Gameの修正
    
    * タイトルからサウンドオプションへの遷移（Xボタンで遷移）
    
    * QTEの追加及び、Game内のフェーズの追加と細かな修正
    
    * Gameの修正
    
    * QTEの修正
    
    * QTEの修正１
    
    * QTEの修正
    
    * Sound本背景の実装
    
    * Sound背景アセット
    
    * GameClear本画面のアセットの追加
    
    * PauseMenuの背景の本実装と、文字フォントの変更
    
    * 本番用のステージの追加　
    
    * ポートフォリオの追加
    
    * ポートフォリオの修正
    
    * Youtubeリンクの追加
    
    * ポートフォリオの修正
    
    * htmlファイルの追加
    
    * 操作説明の追加（後にルール説明も追加）
    
    * 操作説明のアセットの追加
    
    * 操作画面の修正
    
    * ビルド内に追加（動作確認済み）
    
    * 操作説明の修正とタイトルのメニューと選択できるように追加
    
    * メニューの修正
    
    * ビルドの追加
    
    * TitleMenuのちらつきを修正したものと、ルール説明の追加（1枚だけなので後に追加していきます）
    
    * ルールボタンが機能していなかったので修正
    
    * ルール追加の実装
    
    * GameClear,overにメニューを追加したのとGameCameraを修正とQTE用のカメラの追加
    
    * Menuのビルドを追加
    
    * 成功回数の条件を達成したらゲームクリア画面を、失敗したらゲームオーバー画面を反映＋ゲームループの実装
    
    * Loading画面の追加（後にルール説明等もLoading画面で見えるように表記予定）
    
    ---------
    
    Co-authored-by: AokiMutsuki <aokiMutsuki@gmail.com>

---
+++ Game/Loading.h
@@ -0,0 +1,77 @@
+#pragma once
+
+enum class LoadingPhase
+{
+	Gear1,
+	Gear2,
+	Gear3,
+	Loop
+};
+
+class Loading : public IGameObject
+{
+public:
+	Loading() {}
+	~Loading() {}
+	bool Start();
+	void Update();
+	void Render(RenderContext& renderContext);
+
+public:
+	///////// LodingUI関連の処理////////
+
+	/// <summary>
+	/// Loading開始から終了までのメータ処理
+	/// </summary>
+	void LoadingMate();
+
+    /// <summary>
+	/// メーターを時間計測に沿って伸ばしていく処理
+    /// </summary>
+	void UpdateBar();
+
+	/// <summary>
+	/// Loading文字を点滅表示する処理
+	/// </summary>
+	void LoadingLetter();
+
+private:
+
+	Vector3 m_position;
+	SpriteRender m_Sprite;
+	SpriteRender m_CircleRender;
+	SpriteRender m_baseBar;
+	SpriteRender m_fillBar;
+	SpriteRender m_Letter;
+	std::unique_ptr<SpriteRender> m_lightSprite;
+
+	/// <summary>
+	/// 開始時間を記録
+	/// </summary>
+	std::chrono::steady_clock::time_point m_startTime;
+
+private:
+
+	
+	float m_angle = 0.0f;
+	float m_barWidth = 580.0f;
+	float m_elapsedTime = 0.0f;   // 経過時間
+	float m_totalTime = 5.0f;     // ロード完了までの時間（秒）
+	float m_progress = 0.0f;      // 0.0～1.0
+	bool  m_isFinished = false;
+	float radius = 350.0f;
+
+	LoadingPhase m_phase = LoadingPhase::Gear1;
+	float m_animTimer = 0.0f;
+	float m_animInterval = 0.2f;
+
+	////////遅延、待機時間処理////////
+
+	float m_finishTimer = 0.0f;
+	float m_finishWaitTime = 1.0f; // 1秒待つ
+	bool m_isWaiting = false;
+	float m_displayProgress = 0.0f;
+
+};
+
+
